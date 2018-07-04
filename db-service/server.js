bb9c483b417cb4993d3d46185cfd2c9263e154c3
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-07-04 15:39:33 +0330

    refactoring files

--- db-service/server.js
+++ db-service/server.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
 
 
@@ -11,20 +8,8 @@ const User = require('./model');
 
 require('./conection');
 
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.listen(PORT , () => {
     console.log(`Waiting db-service for requestes on port : ${PORT}`)
 });
 
-app.route('/').get((req,res) => {
-    res.send('Hello to db-service');
-});
-
-app.route('/user').post((req,res) => {
-    console.log(req.body);
-});
