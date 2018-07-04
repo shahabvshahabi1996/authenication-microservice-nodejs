e6277c9fc2dada3f0a4942082c1379b931dc978d
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-07-04 15:39:12 +0330

    added bodyparser and cors to the server

--- user-service/server.js
+++ user-service/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 
 const PORT = 3000 || process.env.PORT;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.listen(PORT , () => {
