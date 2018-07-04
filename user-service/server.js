3db09504921365eaaf23ff0a5973485751cf865d
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-07-04 16:17:03 +0330

    fixing bugses

--- user-service/server.js
+++ user-service/server.js
@@ -3,15 +3,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-
-const PORT = 3000 || process.env.PORT;
+const routes = require('./routes');
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'view')));
+const PORT = 3000 || process.env.PORT;
 
+routes(app);
 
 app.listen(PORT , () => {
     console.log(`Waiting user-service for requestes on port : ${PORT}`)
 });
+
