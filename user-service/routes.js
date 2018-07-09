12e5866548f76217735aac3cab268856fc22f3ee
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-07-10 01:40:42 +0330

    refactoring files

--- user-service/routes.js
+++ user-service/routes.js
@@ -1,5 +1,14 @@
+const contorller = require('./contorller');
 module.exports = (app) => {
     app.route('/').get((req,res) => {
         res.send('Hello to user-service');
-    })
+    });
+
+    app.route('/signup').post(
+        contorller.signupUser
+    )
+
+    app.route('/login').post(
+        contorller.loginUser       
+    )
 }
--- user-service/server.js
+++ user-service/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
 const routes = require('./routes');
+const path = require('path');
+var expressValidator = require('express-validator');
 
 app.use(cors());
-
+app.use(expressValidator());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'view')));
