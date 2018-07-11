73b28e3193fc0cbec20998075da2cc65fabd2190
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-07-11 03:32:55 +0330

    added new routes

--- user-service/routes.js
+++ user-service/routes.js
@@ -1,14 +1,22 @@
 const contorller = require('./contorller');
 module.exports = (app) => {
-    app.route('/').get((req,res) => {
-        res.send('Hello to user-service');
-    });
+    app.route('/').get(
+        contorller.home
+    );
+
 
     app.route('/signup').post(
         contorller.signupUser
+    )
+    app.route('/signup').get(
+        contorller.signupPage
     )
 
     app.route('/login').post(
         contorller.loginUser       
     )
+    app.route('/login').get(
+        contorller.loginPage
+    )
+
 }
