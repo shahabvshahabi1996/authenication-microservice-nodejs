f2bc539ebe312d2f92718f445ed1c0e659b48f29
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-07-16 00:08:24 +0330

    added winston config file

---
+++ user-service/winston.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+
+const env = process.env.NODE_ENV || 'development';
+
+const tsFormat = () => (new Date()).toLocaleTimeString();
+exports.logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: 'info'
+    }),
+    new (winston.transports.File)({
+      filename: `user-service.log`,
+      timestamp: tsFormat,
+      level: env === 'development' ? 'debug' : 'info'
+    })
+  ]
+});
